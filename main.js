01737dd63084814bfa0af4b11d3317d4db8dd58d
homeqwert <home1201@gmail.com>
2024-02-01 23:58:50 +0900

    add mobile menu
    모바일 메뉴 기능 추가

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 import './sass/style.scss';
+
+const menuEl = document.querySelector("[data-menu]");
+
+const openMenuBtnEl = document.querySelector("[data-openMenuBtn]");
+openMenuBtnEl.addEventListener('click', () => {
+  menuEl.dataset.menu = 'open';
+});
+
+
+const closeMenuBtnEl = document.querySelector("[data-closeMenuBtn]");
+closeMenuBtnEl.addEventListener('click', () => {
+  menuEl.dataset.menu = 'close';
+})
